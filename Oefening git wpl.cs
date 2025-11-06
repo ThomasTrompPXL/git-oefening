88eae477a90640239293652eb1f922ee81bd9c29
Thomas Tromp <12500311@student.pxl.be>
2025-11-06 13:34:11 +0100

    DEMO 01: De gebruikersnaam wordt gevraagd

---
+++ Oefening git wpl.cs
@@ -0,0 +1,10 @@
+﻿namespace Oefening_wpl
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Gebruikersnaam: ");
+        }
+    }
+}
