3b48e06d79c1f460604fcadeff10cc4c72f505ce
Thomas Tromp <12500311@student.pxl.be>
2025-11-06 13:38:31 +0100

    DEMO 02: Het wachtwoord wordt gevraagd

--- Oefening git wpl.cs
+++ Oefening git wpl.cs
@@ -5,6 +5,9 @@
         static void Main(string[] args)
         {
             Console.Write("Gebruikersnaam: ");
+            string inputUser = Console.ReadLine();
+            Console.Write("Wachtwoord: ");
+            string inputPassword = Console.ReadLine();
         }
     }
 }
